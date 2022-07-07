feddf239c51f88d2be379236b58f1721adecd139
Victor Desctop <vshulpov@gmail.com>
2022-07-08 01:20:08 +0700

    refactor classes

--- service/src/main/java/com/practice/service/controllers/CurrencyController.java
+++ service/src/main/java/com/practice/service/controllers/CurrencyController.java
@@ -1,10 +1,33 @@
 package com.practice.service.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.practice.service.dao.CurrencyDAO;
+import com.practice.service.model.Currency;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/currency")
 public class CurrencyController {
+    private final CurrencyDAO currencyDAO;
 
+    @Autowired
+    public CurrencyController(CurrencyDAO currencyDAO) {
+        this.currencyDAO = currencyDAO;
+    }
+
+    @GetMapping()
+    public String index() {
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id) {
+        return "currencies/show";
+    }
+
+    @PostMapping()
+    public String create(Currency currency) {
+        currencyDAO.save(currency);
+        return "redirect:/currencies";
+    }
 }
