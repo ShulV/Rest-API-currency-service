11ca4cd7c17cda4ccaa4d8c80bbe4fb667505c8a
Victor Desctop <vshulpov@gmail.com>
2022-07-08 00:59:23 +0700

    add DAO classes

---
+++ service/src/main/java/com/practice/service/controllers/DayCurrencyController.java
@@ -0,0 +1,11 @@
+package com.practice.service.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/currency")
+public class DayCurrencyController {
+
+}
