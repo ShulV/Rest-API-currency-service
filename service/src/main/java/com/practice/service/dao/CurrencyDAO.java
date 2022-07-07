338590e7b79fa36e051ac86ad64c8a0b4cc82d73
Victor Desctop <vshulpov@gmail.com>
2022-07-08 00:47:06 +0700

    add currency dao class, add jdbc dependency

---
+++ service/src/main/java/com/practice/service/dao/CurrencyDAO.java
@@ -0,0 +1,42 @@
+package com.practice.service.dao;
+
+import com.practice.service.model.Currency;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class CurrencyDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public CurrencyDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Currency> index() {
+        return jdbcTemplate.query("SELECT * FROM Currency",
+                new BeanPropertyRowMapper<>(Currency.class));
+    }
+
+    public Currency show(int PK_id) {
+        return jdbcTemplate.query("SELECT * FROM Currency WHERE PK_id=?",
+                        new Object[]{PK_id}, new BeanPropertyRowMapper<>(Currency.class))
+                .stream().findAny().orElse(null);
+    }
+
+    public void save(Currency currency) {
+        jdbcTemplate.update("INSERT INTO DayCurrency VALUES(?, ?, ?, ?)", currency.getPK_Id(),
+                currency.getNumCode(), currency.getCharCode(), currency.getName());
+    }
+
+    public void update(int PK_id, Currency updatedCurrency) {
+        jdbcTemplate.update("UPDATE Currency SET numcode=?, charcode=?, name=? WHERE PK_id=?",
+                updatedCurrency.getNumCode(), updatedCurrency.getCharCode(), updatedCurrency.getName(), PK_id);
+    }
+
+    public void delete(int PK_id) {
+        jdbcTemplate.update("DELETE FROM Currency WHERE PK_id=?", PK_id);
+    }
+}
