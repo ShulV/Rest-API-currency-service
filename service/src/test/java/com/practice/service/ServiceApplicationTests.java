f25eaae1c64f51701cff7e248c7e23fffe0c76c8
Victor Desctop <vshulpov@gmail.com>
2022-07-08 00:33:37 +0700

    add additional files

---
+++ service/src/test/java/com/practice/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
