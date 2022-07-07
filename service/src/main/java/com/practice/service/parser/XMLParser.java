c2b6a17f90dff6365f6d43efb62f2846300bee79
Юрий Пирязев <ypiryazev@gmail.com>
2022-07-08 02:40:05 +0700

    parser initialization method added

--- service/src/main/java/com/practice/service/parser/XMLParser.java
+++ service/src/main/java/com/practice/service/parser/XMLParser.java
@@ -1,5 +1,6 @@
 package com.practice.service.parser;
 
+import com.practice.service.model.Currency;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -7,49 +8,17 @@ import org.jsoup.nodes.Element;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class XMLParser {
     public static void main(String[] args) throws IOException {
-        //xmlConnectDaily();
         xmlConnectPeriod(new Date(2022-1900, 0, 1),new Date(2022-1900, 0, 30),"1235");
+        xmlInitializeCurrency();
     }
 
-    public static void xmlConnectDaily() throws IOException {
-
-        Document doc = Jsoup
-                .connect("http://www.cbr.ru/scripts/XML_daily.asp")
-                .userAgent("Chrome/4.0.249.0 Safari/532.5")
-                .referrer("http://www.google.com")
-                .get();
-        doc = Jsoup.parse(String.valueOf(doc));
-
-        for (Element e : doc.select("NumCode")) {
-            System.out.println(e.text());
-
-        }
-        for (Element e : doc.select("CharCode")) {
-            System.out.println(e.text());
-
-        }
-
-        for (Element e : doc.select("Nominal")) {
-            System.out.println(e.text());
-
-        }
-
-        for (Element e : doc.select("Name")) {
-            System.out.println(e.text());
-
-        }
-
-        for (Element e : doc.select("Value")) {
-            System.out.println(e.text());
-
-        }
-    }
     public static void xmlConnectPeriod(Date date1, Date date2, String NameID) throws IOException {
-
 
         DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         System.out.println(dateFormat.format(date1) + " - " + dateFormat.format(date2));
@@ -66,5 +35,49 @@ public class XMLParser {
             System.out.println(e.text());
         }
 
+    }
+
+    public static void xmlInitializeCurrency() throws IOException {
+
+        List<Currency> currenciesList = new ArrayList<>();
+
+        List<String> IDList = new ArrayList<>();
+        List<String> NumCodeList = new ArrayList<>();
+        List<String> CharCodeList = new ArrayList<>();
+        List<String> NameList = new ArrayList<>();
+
+        String html = "http://www.cbr.ru/scripts/XML_daily.asp";
+        Document doc = Jsoup
+                .connect(html)
+                .userAgent("Chrome/4.0.249.0 Safari/532.5")
+                .referrer("http://www.google.com")
+                .get();
+
+        for (Element e : doc.select("Valute")) {
+            IDList.add(e.attr("ID"));
+        }
+        for (Element e : doc.select("NumCode")) {
+            NumCodeList.add(e.text());
+        }
+
+        for (Element e : doc.select("CharCode")) {
+            CharCodeList.add(e.text());
+        }
+
+        for (Element e : doc.select("Name")) {
+            NameList.add(e.text());
+        }
+
+        for(int i = 0; i < NameList.size(); i++){
+            currenciesList.add(new Currency(
+                            IDList.get(i),
+                            NumCodeList.get(i),
+                            CharCodeList.get(i),
+                            NameList.get(i)));
+        }
+
+        for (Currency currency: currenciesList) {
+            System.out.println(currency.toString());
+        }
     }
 }
