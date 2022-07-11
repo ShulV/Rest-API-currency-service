bf48c0731bab822d394a85823798507ca78b2409
Atemyn <atemyn@mail.ru>
2022-07-12 00:41:23 +0700

    Добавлен свой Toolbar

--- android-client/app/src/main/java/com/practice/androidclientapp/MainActivity.java
+++ android-client/app/src/main/java/com/practice/androidclientapp/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Создание своего тулбара и установка центрированного текста.
+        MaterialToolbar materialToolbar = findViewById(R.id.toolbar);
+        materialToolbar.setTitle(R.string.text_MainForm);
+        materialToolbar.setTitleCentered(true);
+        setSupportActionBar(materialToolbar);
     }
 }
