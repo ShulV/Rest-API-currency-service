b02d3df614327d9700f03cfcc8d65869d851ac60
Atemyn <atemyn@mail.ru>
2022-07-13 02:42:37 +0700

    Нижнее меню подключено, но при попытке раскоментировать метод для обработки выбора элемента приложение крашится на моменте запуска

---
+++ android-client/app/src/main/java/com/practice/androidclientapp/ForPeriodFragment.java
@@ -0,0 +1,23 @@
+package com.practice.androidclientapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ForPeriodFragment extends Fragment {
+
+    public ForPeriodFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_for_period, container, false);
+    }
+}
