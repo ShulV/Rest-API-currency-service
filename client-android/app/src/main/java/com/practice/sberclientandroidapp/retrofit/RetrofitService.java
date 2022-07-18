545821638851c01c3ef06a558470e5453ea83d97
Atemyn <atemyn@mail.ru>
2022-07-18 16:04:14 +0700

    [android-client] Ошибка с неправильным форматом даты исправлена; курс валюты за период выводится, но не весь - последние три даты не показываются (скорее всего ошибка с несоответствием размера экрана и фрагмента)

--- client-android/app/src/main/java/com/practice/sberclientandroidapp/retrofit/RetrofitService.java
+++ client-android/app/src/main/java/com/practice/sberclientandroidapp/retrofit/RetrofitService.java
@@ -3,9 +3,17 @@ package com.practice.sberclientandroidapp.retrofit;
 import android.content.res.Resources;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
 import com.practice.sberclientandroidapp.R;
 
 import org.w3c.dom.Text;
+
+import java.lang.reflect.Type;
+import java.sql.Date;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -19,11 +27,20 @@ public class RetrofitService {
     }
     // Method that initializes Retrofit object.
     private void initializeRetrofit() {
+        GsonBuilder builder = new GsonBuilder();
+
+        // Register an adapter to manage the date types as long values
+        builder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>() {
+            public Date deserialize(JsonElement json, Type typeOfT,
+                                    JsonDeserializationContext context) throws JsonParseException {
+                return new Date(json.getAsJsonPrimitive().getAsLong());
+            }
+        });
         retrofit = new Retrofit.Builder()
                 // Server Url address that is defined in string resources.
                 .baseUrl("http://192.168.1.4:8080")
                 // Adding Converter Factory to process json.
-                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .addConverterFactory(GsonConverterFactory.create(builder.create()))
                 .build();
     }
     // Getter method for retrofit field.
