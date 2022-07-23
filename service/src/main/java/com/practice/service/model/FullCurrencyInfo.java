10cb9b8381dfdddf4392e0efb403eb04c459eefe
Юрий Пирязев <ypiryazev@gmail.com>
2022-07-23 22:12:38 +0700

    [Server] Переписан парсинг под новую модель FullCurrencyInfo

--- service/src/main/java/com/practice/service/model/FullCurrencyInfo.java
+++ service/src/main/java/com/practice/service/model/FullCurrencyInfo.java
@@ -6,8 +6,18 @@ public class FullCurrencyInfo {
     private double value;
     private Date date;
     private int nominal;
+
+    private int numCode;
     private String charcode;
     private String name;
+
+    public int getNumCode() {
+        return numCode;
+    }
+
+    public void setNumCode(int numCode) {
+        this.numCode = numCode;
+    }
 
     public double getValue() {
         return value;
@@ -49,10 +59,11 @@ public class FullCurrencyInfo {
         this.name = name;
     }
 
-    public FullCurrencyInfo(double value, Date date, int nominal, String charcode, String name) {
+    public FullCurrencyInfo(double value, Date date, int nominal, int numCode, String charcode, String name) {
         this.value = value;
         this.date = date;
         this.nominal = nominal;
+        this.numCode = numCode;
         this.charcode = charcode;
         this.name = name;
     }
