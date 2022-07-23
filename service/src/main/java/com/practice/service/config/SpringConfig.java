9cea67e0df890e1adc3bf6293b7afb567d8f69b0
Victor Desctop <vshulpov@gmail.com>
2022-07-23 22:44:10 +0700

    [Server] разрешил фронтенду делать CORS-запросы

--- service/src/main/java/com/practice/service/config/SpringConfig.java
+++ service/src/main/java/com/practice/service/config/SpringConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -24,6 +25,20 @@ public class SpringConfig implements WebMvcConfigurer {
         this.applicationContext = applicationContext;
     }
 
+    // разрешение отравлять запросы с других адресов/портов (разрешение CORS)
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowedOrigins("http://localhost:3000/");
+            }
+        };
+
+    }
 
     @Bean
     @Profile("!test")
