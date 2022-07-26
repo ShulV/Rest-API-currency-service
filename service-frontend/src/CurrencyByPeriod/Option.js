27a312593df2fd90494a636705181f8a1eebb751
Victor Desctop <vshulpov@gmail.com>
2022-07-26 19:15:02 +0700

    /period-currencies получает данные с api через форму

--- service-frontend/src/CurrencyByPeriod/Option.js
+++ service-frontend/src/CurrencyByPeriod/Option.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const Option = (props) => {
+    const optionHandler = (event) => {
+        console.log(event.target.dataset.charcode)
+    }
 
     return (
-        <option className='currency-by-period__option'>
+        <option className='currency-by-period__option' data-charcode={props.currency.charcode}>
             {props.currency.name}
         </option>
     )
--- service-frontend/src/CurrencyByPeriod/Select.js
+++ service-frontend/src/CurrencyByPeriod/Select.js
@@ -7,10 +7,8 @@ const Select = (props) => {
 
     return (
         
-        <select className='currency-by-period__select'>
-
+        <select className='currency-by-period__select' onChange={props.selectOptionHandler}>
             { 
-            
                 props.currencies.map((currency, index) => {
                     return (
                      <Option currency={currency} index={index} key={index}/>
